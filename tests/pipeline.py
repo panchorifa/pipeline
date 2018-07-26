44896df1ea9ec32271dad34ecc47b036956172ec
Pancho Rifa <victor@1910.io>
2018-07-26 05:20:33 -0600

    Initial commit

---
+++ tests/pipeline.py
@@ -0,0 +1,23 @@
+./pipeline.py --source-url https://s3.amazonaws.com/dox-hiring-samples/npi-samples/npi-sample-2016-02-07.csv --sink-user root --sink-password password --sink-host mysql --sink-database external --sink-table npi
+
+
+
+
+./pipeline.py
+    --source-url
+        https://s3.amazonaws.com/dox-hiring-samples/npi-samples/npi-sample-2016-02-07.csv
+
+    --sink-user
+        root
+
+    --sink-password
+        password
+
+    --sink-host
+        mysql
+
+    --sink-database
+        external
+
+    --sink-table
+        npi
