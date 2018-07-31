8178558282acf3ee6ba2f1d0db1846904b1ebb09
Pancho Rifa <victor@1910.io>
2018-07-31 07:45:21 -0600

    Adding first test

---
+++ tests/test_pipeline.py
@@ -0,0 +1,28 @@
+# https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
+
+def test_pipeline():
+    assert 'simona' == 'simona'
+
+def test_pipeline2():
+    assert 'simonax' == 'simona'
+
+
+
+# ./pipeline.py
+#     --source-url
+#         https://s3.amazonaws.com/dox-hiring-samples/npi-samples/npi-sample-2016-02-07.csv
+#
+#     --sink-user
+#         root
+#
+#     --sink-password
+#         password
+#
+#     --sink-host
+#         mysql
+#
+#     --sink-database
+#         external
+#
+#     --sink-table
+#         npi
